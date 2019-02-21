2f1aed660cac719c2a96aaf10448b3d40f8a8a33
TG-Techie <TGTechie01@gmail.com>
2019-02-20 22:14:13 -0500

    working

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from displayio import release_displays
 release_displays()
 
 #setup disp
-disp = ili9341.ILI9341(busio.SPI(board.SCK, board.MOSI, board.MISO), dio(board.TFT_DC, 0), dio(board.TFT_CS, 0), dio(board.TFT_RESET, 0))
+disp = ili9341.ILI9341(busio.SPI(board.SCK, board.MOSI, board.MISO), dio(board.TFT_WR, 0), dio(board.TFT_CS, 0), dio(board.TFT_RESET, 0))
 backlight = pulseio.PWMOut(board.TFT_BACKLIGHT)
 backlight.duty_cycle = 2**16 -1
 
@@ -24,7 +24,7 @@ gui.place_text = disp.text
 
 gui.good_text_size = 2
 gui.good_gap_size  = 5
-gui.good_widget_size  = 60
+gui.good_widget_size  = 50
 gui.aprox_char_size = (6,8)
 
 gui.color = rgb.colorili
@@ -39,7 +39,7 @@ system.y = 0
 system.width = disp.width
 system.height = disp.height
 
-system.enable_touch_navigation_bar = False
+system.enable_touch_navigation_bar = True
 system.enable_system_status_bar = True
 
 #system.standard_path = 'programs'
@@ -56,30 +56,4 @@ was_touched = False
 while True:
     #touch sterf
     try:
-        point = ts.touch_point[0:2]
-        system_handler.push_event('ptr.go.'+str(),'ptr.d')
-        was_touched = True
-    except:
-        if was_touched:
-            system_handler.push_event('ptr.prs')
-            was_touched = False
-        else:
-            system_handler.push_event('ptr.u')
-
-
-    #if connected over serial
-    '''if supervisor.runtime.serial_connected:
-        system.repl_query()
-    else:
-        system_handler.push_event('mv')'''
-
-    if '!break' in system_handler._event_que:
-        break
-
-    #the system.cycle runs a cycle then returns any errors and outputs
-    ret = system.cycle()
-    if len(ret):
-        print(ret)
-    gc.collect()
-
-print('done')
+        point = ts.touch_poÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿ
