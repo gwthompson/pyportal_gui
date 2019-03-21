d297884e4f612bb5e0b4b2cec9ffeb5e7c682fa3
TG-Techie <TGTechie01@gmail.com>
2019-03-21 10:01:03 -0400

    updates

--- lib/tg_gui_extens/artsy.py
+++ lib/tg_gui_extens/artsy.py
@@ -37,14 +37,18 @@ class canvas(widget):
                     self.physical_y,
                     self.width, self.height, 0,
                     self._border_color)
-        round_rect( self.physical_x + self._border,
-                    self.physical_y + self._border,
-                    self.width - self._border*2,
-                    self.height - self._border*2,
-                    0, self._color)
+        self.clear()
+
 
     def draw_pixel(self, x, y, color):
         if 0<x<self.width-self._border*2 and 0<y<self.height-self._border*2:
             round_rect( self.physical_x + self._border + x,
                         self.physical_y + self.height -self._border -y,
                         1, 1, 0, color)
+
+    def clear(self):
+        round_rect( self.physical_x + self._border,
+                    self.physical_y + self._border,
+                    self.width - self._border*2,
+                    self.height - self._border*2,
+                    0, self._color)
