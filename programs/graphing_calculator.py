6148cd113d14952b9bde723a4c7130ea3b168489
TG-Techie <TGTechie01@gmail.com>
2019-04-04 16:03:26 -0400

    updates

--- programs/graphing_calculator.py
+++ programs/graphing_calculator.py
@@ -9,6 +9,11 @@ page0 = gui.page()
 graph_canvas = canvas(0,0,page0.width,page0.height*0.8,page0)
 graph_function = 'x**2'
 #wait until canvas is done being made
-for x in range(-10,11,1):
-    print(eval(graph_function))
-    graph_canvas.draw_pixel(scale_position(x,page0.width),scale_position(eval(graph_function),page0.height),gui.red)
+def draw_graph(source):
+    global graph_function
+    for x in range(-10,11,1):
+        print(x)
+        graph_expression = graph_function.replace('x',str(x))
+        graph_canvas.draw_pixel(int(graph_canvas.width/2),int(graph_canvas.height/2),gui.red)
+        graph_canvas.draw_pixel(scale_position(x,graph_canvas.width),scale_position(eval(graph_expression),graph_canvas.height),gui.red)
+graph_canvas.on_place = draw_graph
